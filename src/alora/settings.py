794dac0b7a4157a5b04864887afcb4c33b10a37c
mahd <ahmad_napier@bloomfield.edu>
2020-02-28 19:43:38 -0500

    Converted sqlite database into a MySQL database

--- src/alora/settings.py
+++ src/alora/settings.py
@@ -83,11 +83,14 @@ WSGI_APPLICATION = 'alora.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'aloradb',
+        'USER': 'root',
+        'PASSWORD': 's3cur3p455@aloradevz',
+        'HOST': '3.14.84.203',
+        'PORT': '3306',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 

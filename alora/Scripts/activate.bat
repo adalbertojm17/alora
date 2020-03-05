8773d92b58146e3a04e2db866ec9f171a9410854
mahd <ahmad_napier@bloomfield.edu>
2020-03-05 13:13:31 -0500

    Re-configured venv

--- alora/Scripts/activate.bat
+++ alora/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\denis\PycharmProjects\alora\alora
+set VIRTUAL_ENV=C:\Users\thato\PycharmProjects\alora\alora
 
 if not defined PROMPT set PROMPT=$P$G
 

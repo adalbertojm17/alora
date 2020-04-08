6557aa43afd2be54b9103449820488fb8d787efb
adalb <adalberto_jimenezmartinez@bloomfield.edu>
2020-04-08 00:19:49 -0400

    fix a small problem with the order model

--- src/core/models.py
+++ src/core/models.py
@@ -55,7 +55,7 @@ class Order(models.Model):
     current_status = models.CharField(choices=STATUS_CHOICES, max_length=2, default='P')
 
     def __str__(self):
-        return self.pk + ' ' + self.account.name
+        return str(self.pk) + ' ' + self.account.username
 
     def get_status(self):
         return self.current_status

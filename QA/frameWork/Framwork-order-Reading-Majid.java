7b26ae29639635c4d8f92259ea7e66461118f938
majid masood <majid_masood@bloomfield.edu>
2020-04-17 23:34:22 -0400

    frame work script order page

---
+++ QA/frameWork/Framwork-order-Reading-Majid.java
@@ -0,0 +1,32 @@
+package FrameworkNotepad;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ReadingNotepadData {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+	
+		String TestFile = "C:\\Users\\java\\Desktop\\Framework\\OrderDataNotepad.txt";
+		FileReader FR = new FileReader(TestFile);
+		BufferedReader BR = new BufferedReader(FR);
+		String parameters = "";
+		BR.readLine();		
+				
+		//Loop to read all lines one by one from file and print It.
+		while((parameters = BR.readLine())!= null) {
+			String[] parameter = parameters.split("\\|");
+			String userName = parameter[0];
+			String passWord = parameter[1];
+			
+			loginNotepad	ss = new loginNotepad();
+			
+			ss.login(userName, passWord);
+
+		}
+	}
+}
